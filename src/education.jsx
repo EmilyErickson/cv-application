505b457c56711f18683f5dcb6601179f82d5cfa0
EmilyErickson <emilyerickson2002@gmail.com>
2024-01-23 15:56:06 -0600

    Create logic to allow for multiple education objects in array

--- src/education.jsx
+++ src/education.jsx
@@ -1,75 +1,94 @@
 import { useState } from 'react'
-// import { date } from 
 import './index.css'
+import { v4 as uuidv4 } from 'uuid';
+import PropTypes from 'prop-types';
 
-
-
-export default function Education () {
-    // function getKey() {
-    //     // Need to download UUID package for this
-    //     return(
-    //         new Date().getTime()
-    //     )
-    // }
-    const educationInfo = {    
-        // key: getKey,
-        school: "Georgia Institute of Technology",
-        degree: "Computer Science",
-        startDate: "2013-05",
-        endDate: "2017-05",
-        location: "Atlanta, GA"
+export default function Education({ educationsArray, updateThisEducation, addNewEducation}) {
+    
+    Education.propTypes = {
+        educationsArray: PropTypes.array.isRequired,
+    updateThisEducation: PropTypes.func.isRequired,
+    addNewEducation: PropTypes.func.isRequired,
+    };
+    
+    function handleInputChange(key, inputName, value) {
+        let thisEducation = educationsArray.find(education => education.key === key)
+        updateThisEducation(inputName, value, thisEducation)
+    console.log("testing", value)
     }
 
-    function SchoolInput() {
+    function handleSubmit(e) {
+        e.preventDefault();
+    }
+
+
+    function SchoolInput({educationInfo}) {
+        SchoolInput.propTypes = {
+            educationInfo: PropTypes.object.isRequired
+        }
         return (
         <>
             <form className="school-input">
                 <label htmlFor="school-input">School Name</label>
-                <input type="text" value={educationInfo.school}/>
+                <input id='school-input' type="text" defaultValue={educationInfo.school}
+                onBlur={(e) => handleInputChange(educationInfo.key, 'school', e.target.value)}></input>
             </form>
         </>
         )
     }
 
-    function DegreeInput() {
+    function DegreeInput({educationInfo}) {
+        DegreeInput.propTypes = {
+            educationInfo: PropTypes.object.isRequired
+        }
         return (
         <>
             <form className="degree-input">
                 <label htmlFor="degree-input">Degree</label>
-                <input type="text" value={educationInfo.degree}/>
+                <input id='degree-input' type="text" defaultValue={educationInfo.degree}
+                onBlur={(e) => handleInputChange(educationInfo.key, 'degree', e.target.value)}></input>
             </form>
         </>
         )
     }
 
-    function StartDateInput() {
+    function StartDateInput({educationInfo}) {
+        StartDateInput.propTypes = {
+            educationInfo: PropTypes.object.isRequired
+        }
         return (
         <>
             <form className="date-input">
                 <label htmlFor="start-date-input">Start Date</label>
-                <input type="month" value={educationInfo.startDate}/>
+                <input id='start-date-input' type="month" defaultValue={educationInfo.startDate}/>
             </form>
         </>
         )
     }
 
-    function EndDateInput() {
+    function EndDateInput({educationInfo}) {
+        EndDateInput.propTypes = {
+            educationInfo: PropTypes.object.isRequired
+        }
         return (
         <>
             <form className="date-input">
                 <label htmlFor="end-date-input">End Date</label>
-                <input type="month" value={educationInfo.endDate}/>
+                <input id='end-date-input' type="month" defaultValue={educationInfo.endDate}/>
             </form>
         </>
         )
     }
 
-    function LocationInput() {
+    function LocationInput({educationInfo}) {
+        LocationInput.propTypes = {
+            educationInfo: PropTypes.object.isRequired
+        }
         return (
         <>
             <form className="location-input">
                 <label htmlFor="location-input">Location</label>
-                <input type="text" value={educationInfo.location}/>
+                <input id='location-input' type="text" defaultValue={educationInfo.location}/>
             </form>
         </>
         )
@@ -78,25 +97,31 @@ export default function Education () {
     function AddButton() {
         return (
             <>
-                <button className='add-btn' onClick={addComponent}>Add Education</button>
+                <button className='add-btn' onClick={addEducation}>Add Education</button>
             </>
         )
     }
 
+    function addEducation() {
+        const newEducation = {
+            key: uuidv4(),
+            school: "",
+            degree: "",
+            startDate: "",
+            endDate: "",
+            location: ""
+        
+        };
 
-    const [components, setComponents] = useState([EducationForm])
+        addNewEducation(newEducation)
+    }
 
-    function addComponent() {
-        let newComponent = <EducationForm key={components.length} />
-        setComponents([...components, newComponent])
-    }  
 
-    //     function addComponent() {
-//         let newComponent =<ExperienceForm key={components.length}/>
-//         setComponents([...components, newComponent])
-//     }  
+    function EducationForm({ educationInfo}) {
 
-    function EducationForm() {
+        EducationForm.propTypes = {
+            educationInfo: PropTypes.object.isRequired,
+        }
 
         const [isActive, setIsActive] = useState(true);
 
@@ -105,64 +130,33 @@ export default function Education () {
           };
 
         return (
-            <div className={isActive ? 'active education-form form' : 'inactive education-form form'}>
+            <div id={educationInfo.key} className={isActive ? 'active education-form form' : 'inactive education-form form'}>
                 <button className='btn' onClick={toggleClass}><svg className='eye' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>eye-outline</title><path d="M12,9A3,3 0 0,1 15,12A3,3 0 0,1 12,15A3,3 0 0,1 9,12A3,3 0 0,1 12,9M12,4.5C17,4.5 21.27,7.61 23,12C21.27,16.39 17,19.5 12,19.5C7,19.5 2.73,16.39 1,12C2.73,7.61 7,4.5 12,4.5M3.18,12C4.83,15.36 8.24,17.5 12,17.5C15.76,17.5 19.17,15.36 20.82,12C19.17,8.64 15.76,6.5 12,6.5C8.24,6.5 4.83,8.64 3.18,12Z" /></svg></button>
                 <span className={isActive ? 'hidden' : 'show school-name'}>{educationInfo.school}</span>
-                <SchoolInput className={isActive ? 'show' : 'hidden'}/>
+                <SchoolInput className={isActive ? 'show' : 'hidden'} educationInfo={educationInfo}/>
                     
-                <DegreeInput />
+                <DegreeInput educationInfo={educationInfo} />
 
-                <StartDateInput />
+                <StartDateInput educationInfo={educationInfo}/>
 
-                <EndDateInput />
+                <EndDateInput educationInfo={educationInfo}/>
 
-                <LocationInput />
+                <LocationInput educationInfo={educationInfo}/>
+                <button type="submit" onClick={handleSubmit}>Save</button>
             </ div>
         )
     }
 
 
    return (
-    <div className='education' >
-        {components.map((i) => (<EducationForm key={i}/>))}
-        <AddButton />
-    </div>
+    <>
+    {educationsArray.map((education, index) => (
+        <div key={education.key}>
+            <EducationForm educationInfo={education} key={education.key}
+            addNewEducation={(newEducation) => addNewEducation(index, newEducation)} />
+        </div>
+    ))}
+    <AddButton addNewEducation={addNewEducation} />
+    </>
    )
 }
-
-
-//     function ExperienceForm() {
-//         const [isActive, setIsActive] = useState(true);
-
-//         const toggleClass = () => {
-//             setIsActive(!isActive);
-//           };
-
-//         return(
-//         <div className={isActive ? 'active experience-form form' : 'inactive experience-form form'}>
-//         <button className='btn' onClick={toggleClass}><svg className='eye' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>eye-outline</title><path d="M12,9A3,3 0 0,1 15,12A3,3 0 0,1 12,15A3,3 0 0,1 9,12A3,3 0 0,1 12,9M12,4.5C17,4.5 21.27,7.61 23,12C21.27,16.39 17,19.5 12,19.5C7,19.5 2.73,16.39 1,12C2.73,7.61 7,4.5 12,4.5M3.18,12C4.83,15.36 8.24,17.5 12,17.5C15.76,17.5 19.17,15.36 20.82,12C19.17,8.64 15.76,6.5 12,6.5C8.24,6.5 4.83,8.64 3.18,12Z" /></svg></button>
-//         <span className={isActive ? 'hidden' : 'show company-name'}>{experienceInfo.company}</span>
-//         <CompanyInput className={isActive ? 'show' : 'hidden'}/>
-
-//         <PositionInput />
-    
-//         <StartDateInput />
-    
-//         <EndDateInput />
-    
-//         <LocationInput />
-//         </div>  
-//             )
-//         }
-//    return (
-//         <>
-//         {components.map((component) => (
-//             <div key={component.id}>
-//                 <ExperienceForm />
-//             </div>
-//             ))}
-//         <AddButton />
-//         </>
-
-//    )
-// }
