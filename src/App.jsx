fc90e66a1da5a5addbfbc1cfa5154055ee726917
EmilyErickson <emilyerickson2002@gmail.com>
2024-01-23 15:00:45 -0600

    Create function to add new experience to experience array

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,83 @@
-// import { useState } from 'react'
+import { useState } from 'react'
 import './App.css'
-import GeneralInfo from './generalInfoInputs'
+import GeneralInfo from './generalInfo'
 import Education from './education'
 import Experience from './experience'
 import Preview from './preview'
+import { v4 as uuidv4 } from 'uuid';
+
 
 function App() {
-  // GeneralInfo()
+  const [personalInfo, setPersonalInfo] = useState({
+    key: uuidv4(),
+    name: "Jane Smith",
+    title: "Software Development Engineer",
+    email: "jane.l.smith@gmail.com",
+    phone: "987-123-4560",
+    location: "Chicago, USA"
+  });
+  
+  function updatePersonalInfo(newInfo) {
+    console.log("updatePersonalInfo newInfo =", newInfo)
+    setPersonalInfo(newInfo);
+  }
+  
+  const [experiencesInfo, setExperiencesInfo] = useState([    
+    {
+      company: "Tech Innovators, Inc.",
+      position: "Software Development Engineer",
+      startDate: "2017-04",
+      endDate: "2019-02",
+      description: "Developed and maintained robust and scalable web applications using technologies such as React.js, Node.js, and MongoDB. Conducted code reviews to ensure code quality and adherence to coding standards.",
+      key: uuidv4(),
+    }, 
+    {
+      company: "InnovateTech Solutions",
+      position: "Full Stack Software Developer",
+      startDate: "2019-02",
+      endDate: "2022-07",
+      description: " Collaborated with cross-functional teams to gather requirements, analyze needs, and deliver high-quality software solutions. Implemented RESTful APIs for efficient communication between front-end and back-end systems.",
+      key: uuidv4(),
+    }, 
+  ])
+
+  function addNewExperience(newExperience) {
+    const updatedExperiencesInfo = [...experiencesInfo, newExperience];
+    setExperiencesInfo(updatedExperiencesInfo);
+  }
+
+
+
+  function updateThisExperience(inputValue, newInfo, experience) {
+    experience[inputValue] = newInfo
+    setExperiencesInfo([...experiencesInfo])
+  }
+
+  const [educationInfo, setEducationInfo] = useState({    
+      // key: getKey,
+      school: "Georgia Institute of Technology",
+      degree: "Computer Science",
+      startDate: "2013-05",
+      endDate: "2017-05",
+      location: "Atlanta, GA"
+  })
+
+function updateEducationInfo(newEducationInfo) {
+  console.log("updateEducationInfo newInfo =", newEducationInfo)
+  setEducationInfo(newEducationInfo);
+
+}
+
   return (
     <div className='parent-container'>
       <div className='input-form'>
-        <GeneralInfo />
-        <Education />
-        <Experience />
+        <GeneralInfo personalInfo={personalInfo} updatePersonalInfo={updatePersonalInfo}/>
+        <Education educationInfo={educationInfo} updateEducationInfo={updateEducationInfo}/>
+        <Experience experiencesInfo={experiencesInfo} updateThisExperience={updateThisExperience} addNewExperience={addNewExperience}/>
       </div>
-      {/* <div> */}
-        <Preview />
-      {/* </div> */}
+      <div>
+        <Preview personalInfo={personalInfo} educationInfo={educationInfo} experiencesInfo={experiencesInfo} />
+      </div>
     </div>
   )
 }
