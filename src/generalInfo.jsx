e2573c560d73914a553cc19709711d59dbf32a2f
EmilyErickson <emilyerickson2002@gmail.com>
2024-01-23 15:54:13 -0600

    Remove unnecessary console logs

--- src/generalInfo.jsx
+++ src/generalInfo.jsx
@@ -1,37 +1,32 @@
 import './index.css'
-import { useState } from 'react';
-// import DefaultInfo from './defaultInputs';
 import { v4 as uuidv4 } from 'uuid';
+import PropTypes from 'prop-types';
 
 
-export default function GeneralInfo() {
+export default function GeneralInfo({ personalInfo, updatePersonalInfo}) {
 
-  const [personalInfo, setPersonalInfo] = useState({
-    key: uuidv4(),
-    name: "Jane Smith",
-    email: "jane.l.smith@gmail.com",
-    phone: "987-123-4560",
-    location: "Chicago, USA"
-  });
-    // let { personalInfo } = DefaultInfo()
+  function updatePreview(newInfo) {
+    updatePersonalInfo(newInfo);
+  }
+
+  GeneralInfo.propTypes = {
+    personalInfo: PropTypes.object.isRequired,
+    updatePersonalInfo: PropTypes.func.isRequired,
+  };
 
   function handleInputChange(inputName, value) {
-    setPersonalInfo(prevValues => ({
+    updatePersonalInfo(prevValues => ({
       ...prevValues,
       [inputName]: value
     }));
-    console.log(value, uuidv4())
-
   }
+
 
   function handleSubmit(e) {
     e.preventDefault();
-    // Now, you can use inputValues to create a new personalInfo array
     const newPersonalInfo = { ...personalInfo };
     newPersonalInfo.key = uuidv4();
-    // alert({e});
-    console.log(newPersonalInfo)
-    
+    updatePreview(newPersonalInfo)
   }
 
     function NameInput() {
@@ -45,6 +40,19 @@ export default function GeneralInfo() {
             </>
         )
     }
+
+    function TitleInput() {
+      return (
+          <>
+          <div className="title-input">
+          <label htmlFor="title-input">Position Title</label>
+          <input id="title-input" type="text" defaultValue={personalInfo.title}
+          onBlur={(e) => handleInputChange('title', e.target.value)}></input>
+          </div>
+          </>
+      )
+  }
+
 
 
     function EmailInput() {
@@ -64,7 +72,7 @@ export default function GeneralInfo() {
         <>
             <form className="phone-input">
                 <label htmlFor="phone-input">Phone</label>
-                <input type="tel" defaultValue={personalInfo.phone} pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}"
+                <input id='phone-input' type="tel" defaultValue={personalInfo.phone} pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}"
     onBlur={(e) => handleInputChange('phone', e.target.value)}/>
             </form>
         </>
@@ -75,7 +83,7 @@ export default function GeneralInfo() {
         return (
             <form className="location-input">
                 <label htmlFor="location-input">Location</label>
-                <input type="address" defaultValue={personalInfo.location}
+                <input id='location-input' type="address" defaultValue={personalInfo.location}
                 onBlur={(e) => handleInputChange('location', e.target.value)}/>
             </form>
         )
@@ -85,87 +93,16 @@ export default function GeneralInfo() {
 
    return (
     <div className='personal-form form'>
-    <NameInput />
+      <NameInput />
 
-    <EmailInput />
+      <TitleInput />
 
-    <PhoneInput />
+      <EmailInput />
 
-    <LocationInput />
-    <button type="submit" onClick={handleSubmit}>Submit</button>
+      <PhoneInput />
+
+      <LocationInput />
+    <button type="submit" onClick={handleSubmit}>Save</button>
     </ div>
    )
-}
-
-
-// 
-// import React, { useState } from 'react';
-// import './index.css';
-
-// export default function GeneralInfo() {
-//   const [inputValues, setInputValues] = useState({
-//     name: "Jane Smith",
-//     email: "jane.l.smith@gmail.com",
-//     phone: "987-123-4560",
-//     location: "Chicago, USA"
-//   });
-
-//   function handleInputChange(inputName, value) {
-//     setInputValues(prevValues => ({
-//       ...prevValues,
-//       [inputName]: value
-//     }));
-//   }
-
-//   function handleSubmit(e) {
-//     e.preventDefault();
-//     // Now, you can use inputValues to create a new personalInfo array
-//     const newPersonalInfo = { ...inputValues };
-//     console.log(newPersonalInfo); // You can replace this with your logic to update the page or perform other actions
-//   }
-
-//   return (
-//     <div className='personal-form form'>
-//       <form onSubmit={handleSubmit}>
-//         <div className="name-input">
-//           <label htmlFor="name-input">Full Name</label>
-//           <input
-//             type="text"
-//             value={inputValues.name}
-//             onChange={(e) => handleInputChange('name', e.target.value)}
-//           />
-//         </div>
-
-//         <div className="email-input">
-//           <label htmlFor="email-input">E-mail</label>
-//           <input
-//             type="email"
-//             value={inputValues.email}
-//             onChange={(e) => handleInputChange('email', e.target.value)}
-//           />
-//         </div>
-
-//         <div className="phone-input">
-//           <label htmlFor="phone-input">Phone</label>
-//           <input
-//             type="tel"
-//             value={inputValues.phone}
-//             pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}"
-//             onChange={(e) => handleInputChange('phone', e.target.value)}
-//           />
-//         </div>
-
-//         <div className="location-input">
-//           <label htmlFor="location-input">Location</label>
-//           <input
-//             type="address"
-//             value={inputValues.location}
-//             onChange={(e) => handleInputChange('location', e.target.value)}
-//           />
-//         </div>
-
-//         <button type="submit">Submit</button>
-//       </form>
-//     </div>
-//   );
-// }
+  }
